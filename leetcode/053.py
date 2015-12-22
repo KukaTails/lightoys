b075f9df6892f19cb73aca62bc441cb4ef7d51dc
heweidong <heweidong526@gmail.com>
2015-12-22 11:57:58 +0800

    add solutions

---
+++ leetcode/053.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum = nums[0]
+        sum = 0
+        for val in nums:
+            sum = sum + val if sum >= 0 else val
+            max_sum = sum if sum > max_sum else max_sum
+        return max_sum
+
+test = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(Solution().maxSubArray(test))
+
