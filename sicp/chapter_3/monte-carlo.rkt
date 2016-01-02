97359a757d56a2a9353e118fe97b975b6e27b6b4
heweidong <heweidong526@gmail.com>
2016-01-02 23:44:17 +0800

    add monte carlo procedure

---
+++ sicp/chapter_3/monte-carlo.rkt
@@ -0,0 +1,21 @@
+(define rand
+  (let ((x 0))
+    (lambda ()
+      (set! x (rand-update x))
+      x)))
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- iterals-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- iterals-remaining 1) trials-passed))))
+  (iter trials 0))
