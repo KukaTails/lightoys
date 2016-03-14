943e58ca9e8925dc928d140cb6a0131d0844c876
heweidong <heweidong526@gmail.com>
2016-03-14 23:21:22 +0800

    add solutions

--- leetcode/063.py
+++ leetcode/063.py
@@ -4,6 +4,29 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
+        n = len(obstacleGrid)
+        m = len(obstacleGrid[0])
+        dp = [[0] * m for i in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if obstacleGrid[i][j] != 0:
+                    dp[i][j] = 0; continue
+                if i == n - 1 and j == m - 1:
+                    dp[i][j] = 1
+                if i < n - 1:
+                    dp[i][j] += dp[i + 1][j]
+                if j < m - 1:
+                    dp[i][j] += dp[i][j + 1]
+        return dp[0][0]
+
+# The solution below can not be accepted for it use functions.
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
         self.n = len(obstacleGrid)
         self.m = len(obstacleGrid[0])
         self.grid = obstacleGrid
