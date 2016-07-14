967f0ff6129f60c394a8a949be122171b56c5976
KukaTails <heweidong526@gmail.com>
2016-07-14 21:31:24 +0800

    add solutions

--- leetcode/005.py
+++ leetcode/005.py
@@ -1,22 +1,33 @@
 class Solution(object):
-    def isPalindrome(self, x):
+    def longestPalindrome(self, s):
         """
-        :type x: int
-        :rtype: bool
+        :type s: str
+        :rtype: str
         """
-        if x < 0:
-            return False
-        seq = []
-        while x != 0:
-            seq.append(x % 10)
-            x //= 10
-        size = len(seq)
-        i = 0
-        j = size - 1
-        while i <= j:
-            if (seq[i] != seq[j]):
-                return False
+        max_pos,  max_len, s= self.manacher(s)
+        ans = ""
+        for i in range(max_pos - max_len, max_pos + max_len):
+            ans += s[i] if s[i] != "#" else ""
+        return ans
+
+    def manacher(self, s):
+        s = '#' + '#'.join(s) + '#'
+
+        RL = [0] * len(s)
+        MaxRight = 0
+        pos = 0
+        max_pos, MaxLen = 0, 0
+        for i in range(len(s)):
+            if i < MaxRight:
+                RL[i] = min(RL[2 * pos - i], MaxRight - i)
             else:
-                i += 1
-                j -= 1
-        return True
+                RL[i] = 1
+            while i - RL[i] >= 0 and i + RL[i] < len(s) and s[i - RL[i]] == s[i + RL[i]]:
+                RL[i] += 1
+            if RL[i] + i - 1 > MaxRight:
+                MaxRight = RL[i] + i - 1
+                pos = i
+            if RL[i] > MaxLen:
+                MaxLen = RL[i]
+                max_pos = i
+        return [max_pos, MaxLen - 1, s]
