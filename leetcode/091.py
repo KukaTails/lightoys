5882fa75304be33d2e7a0a616f5f5a5dff29bd21
KukaTails <heweidong526@gmail.com>
2016-08-28 22:48:58 +0800

    add solutions

--- leetcode/091.py
+++ leetcode/091.py
@@ -5,25 +5,30 @@ class Solution(object):
         :rtype: int
         """
         len_s = len(s)
-        dp = [0] * len_s
+        dp = [1] * (len_s + 2)
+        if not s: return 0
         has_error = False
         for i in range(len_s):
-            if i == 0 and not 1 <= int(s[i]) <= 26:
-                has_error = True
-            if 1 <= int(s[i]) <= 26 or i >= 1:
-                dp[i] = dp[i-1] if i >= 1 else 1
+            index = i + 2
+            if s[i] == '0':
+                if i == 0 or not (1 <= int(s[i-1]) <= 2):
+                    has_error = True
+                dp[index] = dp[index-2]
             else:
-                dp[i] = dp[i-2] if i >= 1 else 0
-            if i - 1 >= 0 and 1 <= int(s[i-1:i+1]) <= 26 and 1 <= int(s[i]) <= 26 and 1 <= int(s[i-1]) <= 26:
-                if (i + 1 < len_s and 1 <= int(s[i+1]) <= 26) or i + 1 == len_s:
-                    if i - 1 == 0:
-                        dp[i] = 2
+                if i >= 1:
+                    if s[i-1] == '0' or int(s[i-1:i+1]) >= 27:
+                        dp[index] = dp[index-1]
                     else:
-                        dp[i] = max(dp[i-1], dp[i-2] + 1)
-            if i >= 1 and not (1 <= int(s[i-1:i+1]) <= 26 or 1 <= int(s[i]) <= 26 or 1 <= int(s[i-1]) <= 26):
-                has_error = True
-        print(dp)
-        return dp[len_s - 1] if len_s != 0 and not has_error else 0
+                        if i >= 2:
+                            dp[index] = dp[index-1] + dp[index-2]
+                        else:
+                            dp[index] = dp[index-1] + 1
+                else:
+                    dp[index] = 1
+        return dp[len_s+1] if not has_error else 0
 
 
-print(Solution().numDecodings("111"))
+# testing
+test_cases = ['', '1029', '1238648392', '1', '11','111', "1111", '12', '20', '37', '137']
+for test_case in test_cases:
+    print(Solution().numDecodings(test_case))
