5bfe23704062e4b31fcff98228ab8fe395305564
KukaTails <heweidong526@gmail.com>
2016-09-07 00:27:39 +0800

    add solutions

--- leetcode/221.py
+++ leetcode/221.py
@@ -10,10 +10,20 @@ class Solution(object):
         for i in range(rows):
             for j in range(columns):
                 if i == 0 or j == 0:
-                    dp[i][j] = matrix[i][j]
+                    dp[i][j] = int(matrix[i][j])
                 else:
-                    dp[i][j] = min([dp[i-1][j], dp[i][j-1], dp[i-1][j-1]]) + 1
-        return dp[rows-1][columns-1]
-
+                    if int(matrix[i][j]) == 1:
+                        dp[i][j] = min([dp[i-1][j], dp[i][j-1], dp[i-1][j-1]]) + 1
+                    else:
+                        dp[i][j] = 0
+        max_val = 0
+        for i in range(rows):
+            for j in range(columns):
+                max_val = max([max_val, dp[i][j]])
+        return max_val ** 2
 
 # testing
+test_cases = [["10"], ["10100","10111","11111","10010"], ["101101","111111","011011","111010","011111","110111"]]
+
+for case in test_cases:
+    print(Solution().maximalSquare(case))
